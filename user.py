8dea369a7136188446c0efae78776600dc8d00de
Angret Kyi <shwekyiangret1512@gmail.com>
2020-09-20 16:59:14 +0630

    This is storing for user profile

---
+++ user.py
@@ -0,0 +1,7 @@
+# will save for user file id, name, password
+
+class User():
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
